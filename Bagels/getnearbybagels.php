29531ebc73c278a4ab741f9850c3ba4e155318d9
cj1m <cj1mckay@gmail.com>
2016-02-16 22:05:33 +0000

    Can now retrieve nearby bagels!

---
+++ Bagels/getnearbybagels.php
@@ -0,0 +1,32 @@
+<?php
+  $connection = mysqli_connect('mysql.hostinger.co.uk','u986542883_cj1m','rarepepes123','u986542883_bagel');
+
+  $lat = is_number($_POST['lat']);
+  $lng = is_number($_POST['long']);
+  $radius = is_number($_POST['radius']);
+
+  $result = mysqli_query($connection,"SELECT *, ( 6371 * acos( cos( radians($lat) ) * cos( radians( loc_lat ) ) * cos( radians( loc_lng ) - radians($lng) ) + sin( radians($lat) ) * sin( radians( loc_lat ) ) ) ) AS distance FROM BAGELS HAVING distance < $radius ORDER BY distance;");
+
+
+  $rows = array();
+  while ($row = mysqli_fetch_assoc($result)) {
+      $rows[] = $row;
+  }
+  print json_encode($rows);
+
+
+
+
+  //Injection Protection
+  function db_quote($value) {
+      global $connection;
+      return mysqli_real_escape_string($connection,$value);
+  }
+  function is_number($value){
+     if(is_numeric($value)){
+      return (float) $value;
+     }else{
+       exit();
+     }
+  }
+?>
